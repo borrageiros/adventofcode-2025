95cae806dfcb6714baa2d35b1759faf9ebb5fb34
borrageiros <adrianborrageirosmourelos@gmail.com>
2025-12-04 23:42:57 +0100

    Extras of day 1 and 2

--- day1/extra/solve.js
+++ day1/extra/solve.js
@@ -1,4 +1,44 @@
 function solve(inputStr) {
-    return 0;
-}
+    const instructions = inputStr.trim().split("\n");
+    let dial = 50;
+    let count = 0;
 
+    for (const instruction of instructions) {
+        const direction = instruction.charAt(0);
+        const steps = parseInt(instruction.slice(1));
+
+        if (direction === "R") {
+            // Si la direción es R, sumamos los pasos al dial
+            let result = dial + steps;
+            // Si la suma resulta mayor al número maximo del dial (99), restamos 100 y sumamos 1 al contador (el dial dio una vuelta completa)
+            while (result > 99) {
+                result -= 100;
+                // Si la dirección es R, actualizamos el dial antes de sumar el contador
+                dial = result;
+                if (dial !== 0) count++;
+            }
+            // Si la suma no resulta mayor al número maximo del dial (99), actualizamos el dial
+            if (result <= 99) {
+                dial = result;
+            }
+        } else if (direction === "L") {
+            // Si la direción es L, restamos los pasos al dial
+            let result = dial - steps;
+            // Si la resta resulta menor al número minimo del dial (0), sumamos 100 y restamos 1 al contador (el dial dio una vuelta completa)
+            while (result < 0) {
+                result += 100;
+                // Si la dirección es L, actualizamos el dial después de sumar el contador
+                if (dial !== 0) count++;
+                dial = result;
+            }
+            // Si la resta no resulta menor al número minimo del dial (0), actualizamos el dial
+            if (result >= 0) {
+                dial = result;
+            }
+        }
+        // Si el dial apunta al 0, sumamos 1 al contador
+        if (dial === 0) count++;
+    }
+    
+    return count;
+}
--- day2/extra/solve.js
+++ day2/extra/solve.js
@@ -1,4 +1,46 @@
+// Crea un array de partes de un string del tamaño especificado
+function splitInParts(str, size) {
+    let parts = [];
+    for (let i = 0; i < str.length; i += size) {
+        parts.push(str.slice(i, i + size));
+    }
+    return parts;
+}
+
+// Calcular los divisores de un numero
+function dividers(number) {
+    let result = [];
+    for (let i = 1; i <= number; i++) {
+        if (number % i === 0) result.push(i);
+    }
+    return result;
+}
+
+function isInvalid(number) {
+    const numberStr = number.toString();
+    const dividersList = dividers(numberStr.length);
+
+    for (const divider of dividersList) {
+        const parts = splitInParts(numberStr, divider);
+        // Si la longitud de las partes es 1, no es necesario verificar
+        if (parts.length === 1) continue;
+
+        if (parts.every(part => part === parts[0])) return true;
+    }
+}
+
 function solve(inputStr) {
-    return 0;
+    const ranges = inputStr.trim().split(",");
+    let total = 0;
+
+    for (const range of ranges) {
+        const [start, end] = range.split("-");
+        // Recorrer todos los numeros del rango
+        for (let i = Number(start); i <= Number(end); i++) {
+            if (isInvalid(i)) total += i;
+        }
+    }
+
+    return total;
 }
 
